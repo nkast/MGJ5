2283461dc3834086481e871627a9f9b5d507a942
Nick Kastellanos <nkastellanos@gmail.com>
2023-11-30 21:48:31 +0200

    add multiplatform solution

---
+++ src/MGJ5.DesktopGL/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace MGJ5
+{
+    /// <summary>
+    /// The main class.
+    /// </summary>
+    public static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            using (var game = new MGJ5Game())
+                game.Run();
+        }
+    }
+}
---
+++ src/MGJ5.WindowsDX/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace MGJ5
+{
+#if WINDOWS || LINUX
+    /// <summary>
+    /// The main class.
+    /// </summary>
+    public static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            using (var game = new MGJ5Game())
+                game.Run();
+        }
+    }
+#endif
+}
